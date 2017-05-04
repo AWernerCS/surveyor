773ed7a2ef7c972db54f2ecca0e2217055d4c77b
Amanda Werner <awerner@bullhorn.com>
2017-05-04 13:02:22 -0500

    Strawpoll working, need to test where the values are going. Currently not compiling after adding StrawHolder class and references. [ Work Commit ]

--- main/java/com/awernercs/surveyor/controllers/SurveyController.java
+++ main/java/com/awernercs/surveyor/controllers/SurveyController.java
@@ -2,16 +2,17 @@ package com.awernercs.surveyor.controllers;
 
 import com.awernercs.surveyor.models.Survey;
 import com.awernercs.surveyor.models.SurveyOption;
+import com.awernercs.surveyor.models.Strawpoll;
 import com.awernercs.surveyor.models.data.SurveyDAO;
 import com.awernercs.surveyor.models.data.SurveyOptionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
@@ -45,6 +46,7 @@ public class SurveyController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddSurveyForm(@ModelAttribute @Valid Survey newSurvey,
                                        @RequestParam String[] surveyOptionValues,
+                                       @RequestHeader(value="User-Agent") String userAgent,
                                        Errors errors,
                                        Model model) {
 
@@ -56,8 +58,22 @@ public class SurveyController {
             newSurvey.surveyOptions.set(i, new SurveyOption(surveyOptionValues[i]));
         }
 
+        Strawpoll newStraw = new Strawpoll();
+        newStraw.setTitle(newSurvey.getQuestion());
+        newStraw.setOptions(surveyOptionValues);
+        newStraw.setMulti(true);
+
+        // Http Entity code: https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/http/HttpEntity.html
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36");
+        HttpEntity<Strawpoll> entity = new HttpEntity<Strawpoll>(newStraw, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        Strawpoll secondStraw = restTemplate.postForObject("https://strawpoll.me/api/v2/polls", entity, Strawpoll.class);
+
         newSurvey.setIsOpen(true);
         newSurvey.setDateAdded();
+        newSurvey.setStrawHolder(secondStraw);
 
         surveyDao.save(newSurvey);
         return "redirect:";
